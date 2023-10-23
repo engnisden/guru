8676b02a43f196074525105130a037731c066794
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 19:57:19 +0200

    Update register.php

--- backlogic/register.php
+++ backlogic/register.php
@@ -34,20 +34,22 @@ if (mysqli_connect_errno()) {
         if ($stmt->num_rows > 0) {
             // Username already exists
             echo 'Username exists, please choose another!';
-        } else
-            /*{
-                if ($stmt = $con->prepare('INSERT INTO users (name, password, email) VALUES (?, ?, ?)')) {
-                    // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
-                    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-                    $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
-                    $stmt->execute();
-                    echo 'You have successfully registered! You can now login!';
-                } else {
-                    // Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all three fields.
-                    echo 'Could not prepare statement!';
-                }
-            }*/
-            $stmt->close();
+        } else {
+            echo 'hejsan';
+            /* 
+
+            if ($stmt = $con->prepare('INSERT INTO users (name, password, email) VALUES (?, ?, ?)')) {
+                // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+                $stmt->execute();
+                echo 'You have successfully registered! You can now login!';
+            } else {
+                // Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all three fields.
+                echo 'Could not prepare statement!';
+            }
+        */}
+        $stmt->close();
     } else {
         // Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
         echo 'Could not prepare statement!';
