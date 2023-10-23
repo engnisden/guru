f25a0c2e08ad6ab027f1b1e68230cdb3c0543e9b
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 17:50:02 +0200

    Update regandlog.php

--- backlogic/regandlog.php
+++ backlogic/regandlog.php
@@ -34,10 +34,10 @@ if ($stmt = $con->prepare('SELECT id, password FROM users WHERE name = ?')) {
     if ($stmt->num_rows > 0) {
         $stmt->bind_result($id, $password);
         $stmt->fetch();
-        echo 'fann ett namn';
+        //echo 'fann ett namn';
         // Account exists, now we verify the password.
         // Note: remember to use password_hash in your registration file to store the hashed passwords.
-        if (password_verify($_POST['password'], $password)) {
+        if ($_POST['password'] === $password) {
             // Verification success! User has logged-in!
             // Create sessions, so we know the user is logged in, they basically act like cookies but remember the data on the server.
             session_regenerate_id();
