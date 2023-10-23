3b1f420d159bdbad0bf9b1e6bc4081501c5d2f45
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 16:49:32 +0200

    Update regandlog.php

--- backlogic/regandlog.php
+++ backlogic/regandlog.php
@@ -15,6 +15,7 @@ session_start();
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
+    print 'Det blev inge bra';
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 
 } else {
