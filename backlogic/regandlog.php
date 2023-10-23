5ce68547c25b387d972bc844f910f7cd956d1fb0
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 16:48:47 +0200

    Update regandlog.php

--- backlogic/regandlog.php
+++ backlogic/regandlog.php
@@ -7,7 +7,7 @@ require '../../secredret.php';
 
 
 
-
+echo $DATABASE_HOST;
 
 
 session_start();
@@ -17,6 +17,8 @@ $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_
 if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 
+} else {
+    print "databaskoppling fungerade";
 }
 
 /*
