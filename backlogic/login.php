16fa717d3f8e882a319106b89c11c1ca6f80b4c0
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 19:29:33 +0200

    0.1

--- backlogic/login.php
+++ backlogic/login.php
@@ -1,14 +1,8 @@
 <?php
 
-
-
-
 require '../../secredret.php';
 
-
 session_start();
-
-
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
---
+++ backlogic/register.php
@@ -0,0 +1,60 @@
+<?php
+
+
+
+
+require '../../secredret.php';
+
+
+session_start();
+
+
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+    //print 'Det blev inge bra';
+    exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+
+} else {
+
+
+
+    if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+        // Could not get the data that should have been sent.
+        exit('Please complete the registration form!');
+    }
+    // Make sure the submitted registration values are not empty.
+    if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+        // One or more values are empty.
+        exit('Please complete the registration form');
+    }
+    if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+        // Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        $stmt->store_result();
+        // Store the result so we can check if the account exists in the database.
+        if ($stmt->num_rows > 0) {
+            // Username already exists
+            echo 'Username exists, please choose another!';
+        } else {
+            if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
+                // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+                $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+                $stmt->execute();
+                echo 'You have successfully registered! You can now login!';
+            } else {
+                // Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all three fields.
+                echo 'Could not prepare statement!';
+            }
+        }
+        $stmt->close();
+    } else {
+        // Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all 3 fields.
+        echo 'Could not prepare statement!';
+    }
+    $con->close();
+}
+
+?>
