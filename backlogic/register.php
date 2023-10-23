cb4dfbba2ae1043567b63de6498fb3cd87c96075
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 19:53:57 +0200

    Update register.php

--- backlogic/register.php
+++ backlogic/register.php
@@ -4,8 +4,8 @@ require '../../secredret.php';
 
 session_start();
 
-echo 'hej';
-/*
+
+
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
     //print 'Det blev inge bra';
@@ -53,5 +53,5 @@ if (mysqli_connect_errno()) {
     $con->close();
 }
 
-*/
+
 ?>
