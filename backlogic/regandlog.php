918c5a6701050627684090e938b777f7b5b93b04
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 16:51:04 +0200

    Update regandlog.php

--- backlogic/regandlog.php
+++ backlogic/regandlog.php
@@ -10,7 +10,7 @@ require '../../secredret.php';
 echo $DATABASE_HOST;
 
 
-session_start();
+//session_start();
 
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
