bd7652fdf253babc7f78125c3d691b5ffc042bde
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 16:59:55 +0200

    Update regandlog.php

--- backlogic/regandlog.php
+++ backlogic/regandlog.php
@@ -8,7 +8,7 @@ require '../../secredret.php';
 
 session_start();
 
-/*
+
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
     print 'Det blev inge bra';
@@ -17,7 +17,7 @@ if (mysqli_connect_errno()) {
 } else {
     print "databaskoppling fungerade";
 }
-*/
+
 
 /*
 if (!isset($_POST['username'], $_POST['password'])) {
