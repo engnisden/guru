10cd00d3e9fb5bad8ef9bf8a609efb6f6230228b
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 16:17:52 +0200

    0.1

--- backlogic/regandlog.php
+++ backlogic/regandlog.php
@@ -18,5 +18,9 @@ if (mysqli_connect_errno()) {
     exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 
 }
+if (!isset($_POST['username'], $_POST['password'])) {
+    // Could not get the data that should have been sent.
+    exit('Please fill both the username and password fields!');
+}
 
 ?>
