b2ab57d7b9bdcb8557ee0a33bbf870bfd2e10a28
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-23 16:54:18 +0200

    Update regandlog.php

--- backlogic/regandlog.php
+++ backlogic/regandlog.php
@@ -6,7 +6,7 @@
 require '../../secredret.php';
 
 
-//session_start();
+session_start();
 
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
@@ -22,9 +22,18 @@ if (mysqli_connect_errno()) {
 if (!isset($_POST['username'], $_POST['password'])) {
     // Could not get the data that should have been sent.
     exit('Please fill both the username and password fields!');
-} else {
-    echo "Tjena mannen";
+} 
+if ($stmt = $con->prepare('SELECT id, password FROM users WHERE username = ?')) {
+    // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
+    $stmt->bind_param('s', $_POST['username']);
+    $stmt->execute();
+    // Store the result so we can check if the account exists in the database.
+    $stmt->store_result();
+
+
+    $stmt->close();
 }
+
 */
 echo "Filen öppnas"
     ?>
