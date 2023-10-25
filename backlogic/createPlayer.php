a35993a8a24140acd3996ed8691db1a248b9ed05
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-25 11:04:05 +0200

    0.1

--- backlogic/createPlayer.php
+++ backlogic/createPlayer.php
@@ -4,7 +4,8 @@ require '../../secredret.php';
 
 session_start();
 
-echo '' + $_POST['country'] + ' ' + $_POST['charname'] + ' ' + $_POST['avatar'];
+echo 'testar: ';
+echo $_POST['country'] + ' ' + $_POST['charname'] + ' ' + $_POST['avatar'];
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
