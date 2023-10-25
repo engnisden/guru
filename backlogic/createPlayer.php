be4d54c6b0e05612dfade5037be96da6d624c9cf
engnisden <83364808+engnisden@users.noreply.github.com>
2023-10-25 16:32:45 +0200

    0.1

--- backlogic/createPlayer.php
+++ backlogic/createPlayer.php
@@ -4,9 +4,10 @@ require '../../secredret.php';
 
 session_start();
 
+/*
 echo 'testar: ';
 echo $_POST['avatar'], ' ', $_POST['country'], ' ', $_POST['charname'], ' ';
-
+*/
 
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
@@ -35,10 +36,10 @@ if (mysqli_connect_errno()) {
             echo 'Character name exists, please choose another !';
         } else {
             if ($stmt = $con->prepare('INSERT INTO players (charname, countryId, avatar) VALUES (?, ?, ?)')) {
-                // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
                 $stmt->bind_param('sss', $_POST['charname'], $_POST['country'], $_POST['avatar']);
                 $stmt->execute();
                 echo 'You have successfully created a character! You can now play!';
+
             } else {
                 // Something is wrong with the SQL statement, so you must check to make sure your accounts table exists with all three fields.
                 echo 'Could not prepare statement!';
